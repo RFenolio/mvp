04d89d2bc1b9fd828f0f80c699e3f6218859403d
rfenolio <mordoc@gmail.com>
2015-09-21 11:55:48 -0700

    create teaListItemView

---
+++ client/views/teaListItemView.js
@@ -0,0 +1,20 @@
+var teaListItemView = Backbone.Extend.view({
+
+  tag: 'li',
+
+  template: _.template('<p>Tea: <%= tea %> temperature: <%= temperature %> time: <%= time %></p>'),
+
+  initialize: function () {
+    this.render();
+  },
+
+  render: function () {
+    var entry = this.template({
+      tea: this.model.get('name'),
+      temperature: this.model.get('temperature'),
+      time: this.model.get('time')
+    })
+
+    this.$el.html(entry);
+  }
+});
