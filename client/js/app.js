e57fdd50f5bf1c951b8d58469f9c1e3319e87925
rfenolio <mordoc@gmail.com>
2015-09-21 12:26:57 -0700

    create app.js

---
+++ client/js/app.js
@@ -0,0 +1,14 @@
+var app = new AppView({
+  collection: [
+    {
+      name: green,
+      temperature: 180,
+      time: 120
+    },
+    {
+      name: black,
+      temperature: 212,
+      time: 300
+    }
+  ]
+});
