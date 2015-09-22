de037ebe5bfd730ff363a2e0efae4ce52b2733fd
rfenolio <mordoc@gmail.com>
2015-09-22 15:35:37 -0700

    remove stub tea data from client

--- client/js/app.js
+++ client/js/app.js
@@ -1,30 +1,3 @@
-
-// stub data
-var example = new Tea({
-  name: 'example',
-  temperature: 190,
-  time: 5
-});
-
-var green = new Tea({
-  name: 'green',
-  temperature: 180,
-  time: 120
-});
-
-var black = new Tea({
-  name: 'black',
-  temperature: 212,
-  time: 300
-})
-
-var oolong = new Tea({
-  name: 'oolong',
-  temperature: 212,
-  time: 250
-})
-
-
 app = {
   server: 'localhost:8000'
 };
