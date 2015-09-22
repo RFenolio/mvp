f7ffee15d7e5c1a211606f6bb6ff3a20cf254069
rfenolio <mordoc@gmail.com>
2015-09-21 17:20:47 -0700

    add timer when you click on an item

--- client/js/views/teaListItemView.js
+++ client/js/views/teaListItemView.js
@@ -24,6 +24,44 @@ var TeaListItemView = Backbone.View.extend({
   },
 
   clickAction: function () {
-    alert(this.model.get('time'));
+    $('body').append('<div id="timer">Your tea will be ready in <span id="time"></span> minutes!</div>');
+    var display = document.querySelector('#time');
+    console.log(this.model.get('time'));  
+    var timer = new CountDownTimer(this.model.get('time'));
+
+    timer.onTick(format(display)).start();
+
+    function format(display) {
+      return function (minutes, seconds) {
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ':' + seconds;
+      };
+    }
   }
 });
+
+
+// window.onload = function () {
+//     var display1 = document.querySelector('#time1'),
+//         display2 = document.querySelector('#time2'),
+//         timer1 = new CountDownTimer(5),
+//         timer2 = new CountDownTimer(10);
+
+//     timer1.onTick(format(display1)).onTick(restart).start();
+//     timer2.onTick(format(display2)).start();
+
+//     function restart() {
+//         if (this.expired()) {
+//             setTimeout(function () { timer1.start(); }, 1000);
+//         }
+//     }
+
+//     function format(display) {
+//         return function (minutes, seconds) {
+//             minutes = minutes < 10 ? "0" + minutes : minutes;
+//             seconds = seconds < 10 ? "0" + seconds : seconds;
+//             display.textContent = minutes + ':' + seconds;
+//         };
+//     }
+// };
