468d9707f6a6b31f8d9b617a7924e70b1f976308
rfenolio <mordoc@gmail.com>
2015-09-21 20:59:47 -0700

    start connecting client to server

--- client/js/views/teaListItemView.js
+++ client/js/views/teaListItemView.js
@@ -25,7 +25,7 @@ var TeaListItemView = Backbone.View.extend({
 
   clickAction: function () {
     $('#timer').remove();
-    $('#app').append('<div id="timer">Enjoy your tea in <span id="time"></span> minutes!</div>');
+    $('#app').prepend('<div id="timer">Enjoy your tea in <span id="time"></span> minutes!</div>');
     var display = document.querySelector('#time');
     var timer = new CountDownTimer(this.model.get('time'));
 
